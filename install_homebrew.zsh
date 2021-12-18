dd6704133a037c359cf3edbe60cedf0bf813041b
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2021-12-18 23:19:32 +0000

    Updates homebrew setup
    - Only tries to install if brew doesn't already exists.
    - Renamed setup script

---
+++ install_homebrew.zsh
@@ -0,0 +1,15 @@
+#!/usr/bin/env zsh
+
+
+echo "\n [Homebrew Setuip]: STARTING 🚧\n"
+
+if exists brew; then
+	echo "\n [Homebrew Setuip]: Skipping Install 🟢 (Already Installed)\n"
+else
+	echo "\n [Homebrew Setuip]: Installing 🟡\n"
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+brew bundle --verbose
+
+ echo "\n [Homebrew Setuip]: COMPLETE ✅\n"
