54fd1abcc509f7b3f89e5b7d8badb555515f24c5
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2021-12-18 19:54:08 +0000

    Now managing hombrew via Brewfile

---
+++ Brewfile
@@ -0,0 +1,18 @@
+#Taps
+tap "homebrew/bundle"
+tap "homebrew/cask"
+tap "homebrew/cask-fonts"
+tap "homebrew/cask-versions"
+tap "homebrew/core"
+
+# Brews (Packages)
+brew "bat"
+brew "httpie"
+brew "gh"
+
+# Casks (Applications)
+cask "font-fira-code"
+cask "1password"
+cask "visual-studio-code"
+cask "google-chrome"
+cask "google-chrome-dev"
--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -4,14 +4,8 @@
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install httpie
-brew install bat
 
-brew install gh
-brew install visual-studio-code
 
-brew install font-fira-code
-brew install google-chrome
-brew install google-chrome-dev
+brew bundle --verbose
 
  echo "\n [Homebrew Setuip]: COMPLETE \n"
