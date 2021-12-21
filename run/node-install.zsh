f4ffab925b770b22c42ec74debf29e6a0bc23e48
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2021-12-21 12:48:38 +0000

    Update node-install.zsh

--- run/node-install.zsh
+++ run/node-install.zsh
@@ -1,24 +1,24 @@
 #!/usr/bin/env zsh
 
-echo "\n  [Node Setup]: 🚧 STARTING\n"
+echo "\n  [Node Install]: 🚧 STARTING\n"
 
 if exists nvm; then
-	echo "\n  [Node Setup]: 🟢 Skipping NVM Install -> Already Installed [$NVM_BIN]\n"
+	echo "\n  [Node Install]: 🟢 Skipping NVM Install -> Already Installed [$NVM_BIN]\n"
 else
-	echo "\n  [Node Setup]: 🚧 Cloning lukechilds/zsh-nvm\n"
+	echo "\n  [Node Install]: 🚧 Cloning lukechilds/zsh-nvm\n"
 	git clone https://github.com/lukechilds/zsh-nvm.git ~/.zsh-nvm
 fi
 
 
 
-echo "\n  [Node Setup]: 🚧 Installing Latest Node LTS version\n"
+echo "\n  [Node Install]: 🚧 Installing Latest Node LTS version\n"
 nvm use
 nvm install
 corepack disable
 
 
 
-echo "\n  [Node Setup]: 🚧 Installing Global Packages\n"
+echo "\n  [Node Install]: 🚧 Installing Global Packages\n"
 npm install --global @sanity/cli
 npm install --global browser-sync
 npm install --global netlify-cli
@@ -26,11 +26,11 @@ npm install --global supabase
 npm install --global vercel
 npm install --global typescript
 
-echo "\n  [Node Setup]: 🟢 Global packages installed\n"
+echo "\n  [Node Install]: 🟢 Global packages installed\n"
 npm list --global --depth=0
 
 mkdir ~/Developer -p
 
-echo "\n  [Node Setup]: ✅ COMPLETE\n"
+echo "\n  [Node Install]: ✅ COMPLETE\n"
 
 $(yarn global bin)
