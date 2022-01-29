2862a92e338bbecd7261b760da852fc21ec587eb
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2022-01-29 08:58:35 +0000

    Fixing install script

--- setup/zsh.zsh
+++ setup/zsh.zsh
@@ -2,7 +2,7 @@
 
 echo "\n [ZSH]: 🚧 STARTING\n"
 
-zsh ~.dotfiles/setup/util/zsh/install.zsh
-zsh ~.dotfiles/setup/util/zsh/config.zsh
+zsh ~/.dotfiles/setup/util/zsh/install.zsh
+zsh ~/.dotfiles/setup/util/zsh/config.zsh
 
 echo "\n  [ZSH]: ✅ COMPLETE\n"
